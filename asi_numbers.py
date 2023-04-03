4ce4c9f6c3b3f8a16818be041312879cfa4f674a
firstname lastname <asiebeheakey@gmail.com>
2023-04-03 12:14:23 +0000

    I'm tracking my project files

---
+++ asi_numbers.py
@@ -0,0 +1,26 @@
+print(3+2)
+print(3**7)
+print(0.5+0.3)
+print(0.7-1)
+print(4/2)
+universe_age = 14_000_000_000
+print(universe_age)
+favorite = "7"
+question = "What is my favorite number?"
+print(question)
+answer = f"My favorite number is {favorite}" 
+print(answer)
+#say hello to everyone
+print("Hello everyone!")
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[3].title())
+print(bicycles[2])
+message = f"My first bicycle was a {bicycles[2].title()}."
+print(message)
+names = ['korkor', 'benedicta', 'mama lotty', 'alexander']
+message_1 = f"Say hello to {names[0].title()}"
+print(message_1)
+question_1 = f"How do you know {names[2].title()}?"
+print(question_1)
+answer_1 = f"{names[2].title()} was my mother"
+print(answer_1)
