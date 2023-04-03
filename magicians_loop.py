549fc9beea62a5eda770e5d8f77ea9b5545fbd0e
firstname lastname <asiebeheakey@gmail.com>
2023-04-03 16:55:37 +0000

    new work

--- magicians_loop.py
+++ magicians_loop.py
@@ -6,7 +6,8 @@ for magician in magicians:
 print("Thank you for a great show!.\n")
 pets = ['cat', 'dog', 'fish']
 for pet in pets:
-    print(f"I have a {pet.title()} as a pet.\n")
+    print(f"I have a {pet.title()} as a pet")
+    print(f"I love my {pet}!.\n")
 
 for value in range(1,6):
     print(value)
@@ -14,3 +15,13 @@ numbers = list(range(1,6))
 print(numbers)
 even_numbers = list(range(2, 15, 2))
 print(even_numbers)
+
+players = ['jet', 'fii', 'kool', 'del']
+print(players[0:3])
+print(players[:3])
+print(players[-3:])
+print("These are the players on my team:")
+for player in players:
+    print(player.title())
+players.append('mimi')
+print(players)
---
+++ new.py
@@ -0,0 +1,5 @@
+wishlist = [['ransbern', 'jesse', 'kofi'], ['waakye', 'jollof', 'banku'], '0266466868']
+attendee = wishlist[0][0]
+food = wishlist [1][2]
+phone_number = wishlist[2]
+print(f"{attendee.title()} prefers {food}. You can reach him on {phone_number}.")
