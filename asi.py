3fff421ba22ee8776c5db9225a1c029bcbb45751
firstname lastname <asiebeheakey@gmail.com>
2023-04-17 17:06:45 +0000

    new projects

---
+++ asi.py
@@ -0,0 +1,58 @@
+# num_tries = 3
+# password = 'funfilled'
+
+# while True:
+#     user_input = input('Please enter your password: ')
+#     if user_input == password:
+#         print('Access granted!')
+#         break
+#     elif user_input != password:
+#         print('Access denied! Try again.')
+#         num_tries = num_tries - 1
+#         print(num_tries)
+#         if num_tries == 0:
+#             print('Device locked. Try again in 30 minutes!')
+#             break
+
+# number = 73
+# number_of_tries = 3
+# while True:
+#     user_input = input('Please enter a number between 0 and 100: ')
+#     if int(user_input) < number:
+#         number_of_tries = number_of_tries - 1
+#         print("Number is low!")
+#         if number_of_tries == 0:
+#             print("Device temporarily locked!")
+#             break
+#     elif int(user_input) > number:
+#         number_of_tries = number_of_tries - 1
+#         print("Number is high!")
+#         if number_of_tries == 0:
+#             print("Device temporarily locked!")
+#             break
+#     else:
+#         print("You're correct!!")
+#         break
+
+
+number = 73
+number_of_tries = 3
+
+while number == 73:
+    user_input = input('Please enter a number between 0 and 100: ')
+    if int(user_input) < number:
+        print("Number is low!")
+        number_of_tries = number_of_tries - 1
+        if number_of_tries == 0:
+            print("Device temporarily locked!")
+            break
+    elif int(user_input) > number:
+        print("Number is high!")
+        number_of_tries = number_of_tries - 1
+        if number_of_tries == 0:
+            print("Device temporarily locked!")
+            break
+    else:
+        print("You're correct!!")
+        break
+
--- pythonfour.py
+++ pythonfour.py
@@ -1,22 +1,49 @@
-dictionary = {
-    "wood": "wawa",
-    "description": ["apotracheal","diffuse porous", "tyloses"],
-    "uses":["furniture", "flooring", "moulding"]
+# dictionary = {
+#     "wood": "wawa",
+#     "description": ["apotracheal","diffuse porous", "tyloses"],
+#     "uses":["furniture", "flooring", "moulding"]
+# }
+
+# # dictionary["colour"] ="white"
+
+# dictionary['colour'] = ['white','yellowish']
+
+# # print(dictionary)
+
+# print(f"{dictionary['description'][2].title()} resemble water bubbles.")
+# print('\n')
+
+# print(f"{dictionary['wood'].title()} is used as {dictionary['uses'][1]} material.")
+
+# for x in dictionary['uses']:
+#     print(x)
+   
+#     print(f"{dictionary['wood'].title()} can be used as {x}.")
+
+person={
+    'first_name':'Asi',
+    'last_name':'Ebeheakey',
+    'age':30,
+    'city':['Accra', 'Sefwi Wiawso']
 }
 
-# dictionary["colour"] ="white"
+'''
+# We want to print a sentence that says the ff:
+# Asi Ebeheakey is 30 and she stays in both Accra and Sefwi
 
-dictionary['colour'] = ['white','yellowish']
+# print(f"{person['first_name']} likes bananas.")
+# for x in person['city']:
+#     print(f"{person['first_name']} stays in {x}.")
 
-# print(dictionary)
+#Add an empty space #''# which will separate first_name from last_name after printing.
+'''
 
-print(f"{dictionary['description'][2].title()} resemble water bubbles.")
-print('\n')
+fullname = person['first_name'] + ' ' + person['last_name']
+age = person["age"]
+#Convert dictionary list to actual list as in 'city': ['Accra', 'Sefwi Wiawso'] is a dictionary list. 
+#By defining #city = list(person['city'])#, it has been converted to an actual list which can be indexed. 
+city = list(person["city"])
+Accra = city[0]
+sefwi = city[1]
 
-print(f"{dictionary['wood'].title()} is used as {dictionary['uses'][1]} material.")
-
-for x in dictionary['uses']:
-    print(x)
-   
-    print(f"{dictionary['wood'].title()} can be used as {x}.")
-
+print(f"{fullname} is {age} and she stays in both {Accra} and {sefwi}")
